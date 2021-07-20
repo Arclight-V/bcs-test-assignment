702f9ff239f4522c77d1e2df8f3593618555957e
Arclight-V <vladimtor@gmail.com>
2021-07-20 19:33:05 +0300

    Add the Stock class
    Contains information about the purchased share.
    
    There are no setters for the immutability of the object after receiving data on the share price from the exchange

---
+++ MarketValueOfThePortfolio/src/main/java/bcs/marketvalue/stocks/Stock.java
@@ -0,0 +1,29 @@
+package bcs.marketvalue.stocks;
+
+public class Stock {
+    private String      tickerSymbol;
+    private String      sector;
+    private float       value;
+
+    public Stock() {
+    }
+
+    public Stock(String tickerSymbol, String sector, float value) {
+        this.tickerSymbol = tickerSymbol;
+        this.sector = sector;
+        this.value = value;
+    }
+
+    public String getTickerSymbol() {
+        return tickerSymbol;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public float getValue() {
+        return value;
+    }
+}
+
