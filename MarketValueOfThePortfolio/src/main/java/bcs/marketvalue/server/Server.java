66112da8e74227e1682c835f68c4145f7eab5d88
Arclight-V <vladimtor@gmail.com>
2021-07-20 19:32:46 +0300

    Add the Stock class
    Contains information about the purchased share.
    
    There are no setters for the immutability of the object after receiving data on the share price from the exchange

--- MarketValueOfThePortfolio/src/main/java/bcs/marketvalue/server/Server.java
+++ MarketValueOfThePortfolio/src/main/java/bcs/marketvalue/server/Server.java
@@ -1,4 +1,49 @@
 package bcs.marketvalue.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket serverSocket;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        while (true) {
+            new ClientHandler(serverSocket.accept()).start();
+        }
+    }
+
+    public void stop() throws IOException {
+        serverSocket.close();
+    }
+
+    private static class ClientHandler extends Thread {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public ClientHandler (Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+                in.close();
+                out.close();
+                clientSocket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
